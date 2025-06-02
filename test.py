5a5e7746e3a849457b692c29640d42696cff853e
Jack Huang <Jack-Huang-2020@outlook.com>
2025-06-02 12:01:37 +0800

    Edit with test.py

--- test.py
+++ test.py
@@ -1,2 +1,19 @@
-print("Hello World")
-1 != 1
+def __in_repeat__():
+    print("a")
+
+def repeat(input):
+    times = 0
+    while times < input:
+        times = times + 1
+        print("time(s) = " + str(times))
+        __in_repeat__()
+
+def __main__():
+    user_input = input("Time(s):")
+    if user_input.isdigit():
+        repeat(int(user_input))
+    else:
+        print("非法输入！")
+        __main__()
+
+__main__()
