43ac8843dc537e768931684ccbc242b5ef51a3a0
Jack Huang <Jack-Huang-2020@outlook.com>
2025-08-15 15:28:42 +0800

    demo

---
+++ Fuck you Windows.py
@@ -0,0 +1,16 @@
+while True:
+    while True:
+        print("Welcome to Python!")
+        if 1 == 1:
+            if 1 != 1:
+                if 1 == 2:
+                    if 1 == 3:
+                        print(1)
+                    else:
+                        print(2)
+                else:
+                    print(3)
+            else:
+                print(4)
+        else:
+            print(5)
--- test.py
+++ test.py
@@ -1,29 +1,37 @@
 def __in_repeat__():
     print("Debug")
+    # Debug Mode
 
 def illegal_entry():
+    # 非法输入
     print("Illegal entry!")
     print("")
     print("==================== Restart ====================")
     print("")
 
 def repeat(def_input):
+    # 用户输入
     times = 0
     debug_mode = input("Open Debug Mode?(Y/N):")
+    # 是否开启
     if debug_mode not in "YNyn":
+        # 检查是否为非法输入
         illegal_entry()
         __main__()
     elif debug_mode in "Yy":
+        # 是
         while times < def_input:
             times = times + 1
             print("time(s) = " + str(times))
             __in_repeat__()
     else:
+        # 否
         while times < def_input:
             times = times + 1
             __in_repeat__()
 
 def __main__():
+    # 主程序
     user_input = input("Time(s):")
     if user_input.isdigit():
         repeat(int(user_input))
@@ -35,3 +43,4 @@ def __main__():
 
 if __name__ == __main__():
     __main__()
+    # 运行
